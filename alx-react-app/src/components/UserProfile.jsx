762835ad12b9b726cd8752d1cadc1ab890a26e18
wendwesen-girma <wendwesengirma89@gmail.com>
2024-09-02 18:18:18 +0300

    Add UserProfile component with props

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+
+
+const UserProfile=(props)=>{
+
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <p>{props.age}</p>
+            <p>{props.bio}</p>
+
+        </div>
+    )
+}
+export default UserProfile;
