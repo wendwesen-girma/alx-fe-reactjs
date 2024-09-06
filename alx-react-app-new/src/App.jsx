5317419cdb616eaf8a6e9f53864e31528f876ec6
wendwesen-girma <wendwesengirma89@gmail.com>
2024-09-06 13:44:20 +0300

    Add Counter component using useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,19 +1,22 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import MainContent from "./components/MainContent"
-// import WelcomeMessage from "./components/WelcomeMessage"
-import UserProfile from './components/UserProfile'
+// import Header from "./components/Header"
+// import Footer from "./components/Footer"
+// import MainContent from "./components/MainContent"
+// // import WelcomeMessage from "./components/WelcomeMessage"
+// import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 function App() {
   
 
   return (
     <div>
-        <h1>Welcome to the User Profile App</h1>
+        {/* <h1>Welcome to the User Profile App</h1> */}
         {/* Example of using the UserProfile component with props */}
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+        {/* <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
         <Header/>
         <Footer/>
-        <MainContent/>
+        <MainContent/> */}
+        <h1>Simple Counter App</h1>
+        <Counter/>
         {/* <WelcomeMessage /> */}
        
     </div>
